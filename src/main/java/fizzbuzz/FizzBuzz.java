4c9554b69165212882f908888b085aecebb6819a
nick bouldien <nick.bouldien@gmail.com>
2020-09-22 16:43:24 -0600

    finish step 3, delete unused file

--- src/main/java/fizzbuzz/FizzBuzz.java
+++ src/main/java/fizzbuzz/FizzBuzz.java
@@ -6,36 +6,52 @@ package fizzbuzz;
 // 3. 'buzz' for numbers that are multiples of 5
 // 4. 'fizzbuzz' for numbers that are multiples of 15
 
+import java.util.HashMap;  
+
 public class FizzBuzz {
     public static void main(String[] args) {
-        Greeter greeter = new Greeter();
-        System.out.println(greeter.sayHello());
-
         int num = 20;
 
         run(num);
     }
 
     private static void run(int num) {
+        HashMap<String, Integer> result = new HashMap<String, Integer>();
+
+        result.put("lucky", 0);
+        result.put("fizzbuzz", 0);
+        result.put("fizz", 0);
+        result.put("buzz", 0);
+        result.put("integer", 0);
+
         for (int i = 1; i <= num; i++) {
             if (hasDigit(3, i)) {
+                result.put("lucky", result.get("lucky") + 1);
                 System.out.println("lucky");
             } else {
                 if (i % 15 == 0) {
                     // multiple of 5 && 7
+                    result.put("fizzbuzz", result.get("fizzbuzz") + 1);
                     System.out.println("fizzbuzz");
                 } else if (i % 3 == 0) {
                     // multiple of 3
+                    result.put("fizz", result.get("fizz") + 1);
                     System.out.println("fizz");
                 } else if (i % 5 == 0) {
                     // multiple of 5
+                    result.put("buzz", result.get("buzz") + 1);
                     System.out.println("buzz");
                 } else {
                     // other
+                    result.put("integer", result.get("integer") + 1);
                     System.out.println(i);
                 }
             }
         }
+
+        result.entrySet().forEach(entry -> {
+           System.out.println(entry.getKey() + ": " + entry.getValue());
+        });
     }
 
     public static boolean hasDigit(int digit, int number) {
--- src/main/java/fizzbuzz/Greeter.java
+++
@@ -1,7 +0,0 @@
-package fizzbuzz;
-
-public class Greeter {
-    public String sayHello() {
-        return "Hello world!";
-    }
-}
